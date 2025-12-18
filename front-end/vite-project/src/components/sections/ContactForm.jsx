414fdadb93f1f27c1990d644f50e33ad63f2ea01
ayii365 <ayii365madut@gmail.com>
2025-12-18 16:46:00 +1030

    Setup formspree account and implemented into form and tested. Works successfully after inputing valid information then submitting

--- front-end/vite-project/src/components/sections/ContactForm.jsx
+++ front-end/vite-project/src/components/sections/ContactForm.jsx
@@ -1,36 +1,40 @@
 import "../../styles/ContactForm.css"
 
-function ContactIntro() {
+function ContactForm() {
     return (
         <div className="contact-form">
             <div className="container">
                 <div className="contact-form-inner">
                     <h1 className="form-heading">Or fill in the form below</h1>
-                    <form className="contact-input-form">
+                    <form 
+                        className="contact-input-form" 
+                        action="https://formspree.io/f/mlgrroeq"
+                        method="POST"
+                    >
 
                         <div className="input-lvl">
                             <div className="input-section">
-                                <label className="form-labels">First Name:</label>
-                                <input className="form-input" type="text"></input>
+                                <label className="form-labels" htmlFor="firstName">First Name:</label>
+                                <input id="firstName" name="firstName" className="form-input" type="text" required></input>
                             </div>
 
                             <div className="input-section">
-                                <label className="form-labels">Last Name:</label>
-                                <input className="form-input" type="text"></input>
+                                <label className="form-labels" htmlFor="lastName">Last Name:</label>
+                                <input id="lastName" name="lastName" className="form-input" type="text" required></input>
                             </div>
                         </div>
 
                         <div className="input-lvl">
                             <div className="input-section input-email">
-                                <label className="form-labels">Email:</label>
-                                <input className="form-input" type="text"></input>
+                                <label className="form-labels" htmlFor="email">Email:</label>
+                                <input id="email" name="email" className="form-input" type="email" required></input>
                             </div>
                         </div>
 
                         <div className="input-lvl">
                             <div className="input-section input-msg">
-                                <label className="form-labels">Message:</label>
-                                <textarea className="form-input" type="text"></textarea>
+                                <label className="form-labels" htmlFor="message">Message:</label>
+                                <textarea id="message" name="message" className="form-input" type="text" required></textarea>
                             </div>
                         </div>
 
@@ -44,4 +48,4 @@ function ContactIntro() {
     );
 }
 
-export default ContactIntro;
+export default ContactForm;
